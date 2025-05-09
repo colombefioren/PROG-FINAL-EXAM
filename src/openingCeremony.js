566a3266e1a75ae92699a3aefd1acd47d79cb90b
Jonathan <rakotoarisoajonathan0@gmail.com>
2025-05-09 13:41:50 +0300

    refactore: improve variable name for better user comprehension

--- src/openingCeremony.js
+++ src/openingCeremony.js
@@ -1,30 +1,14 @@
 const getLastDelegation = (delegations) => {
-  let delegationFullInfo = [];
-
+  let delegationInfo = [];
   for (const delegation of delegations) {
-    const [name, completeDistance, velocity] = delegation.split(" ");
-    delegationFullInfo.push({
+    const [name, distanceToTarget, velocity] = delegation.split(" ");
+    delegationInfo.push({
       name: name,
-      timeTakenToTarget: parseInt(completeDistance) / parseInt(velocity),
+      timeTakenToTarget: parseInt(distanceToTarget) / parseInt(velocity),
     });
   }
-
-  delegationFullInfo.sort(
+  delegationInfo.sort(
     (a, b) => a["timeTakenToTarget"] - b["timeTakenToTarget"]
   );
-
-  return delegationFullInfo[delegationFullInfo.length - 1]["name"];
+  return delegationInfo[delegationInfo.length - 1]["name"];
 };
-
-console.log(
-  getLastDelegation([
-    "Chilli 101 2",
-    "Estonia 58 1",
-    "Romania 102 3",
-    "Ecuador 103 4",
-    "Madagascar 104 5",
-    "Oman 50 1",
-    "Nicaragua 51 3",
-    "Yemen 53 4",
-  ])
-);
