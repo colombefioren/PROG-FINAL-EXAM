545e2340f2a73c88213b898441093c9129a0bf88
Whipander <mihaja2506@gmail.com>
2025-05-09 15:01:28 +0300

    chore: update test

--- test/reduceDirections.test.js
+++ test/reduceDirections.test.js
@@ -3,12 +3,12 @@ import { describe } from "mocha";
 import { reduceDirections } from "../src/reduceDirections.js";
 
 describe("Reduce to the shortest possible way", () => {
-  it("should return OUEST", () => {
+  it("should return ['OUEST']", () => {
     expect(
       reduceDirections(["NORD", "SUD", "SUD", "EST", "OUEST", "NORD", "OUEST"])
     ).to.deep.eq(["OUEST"]);
   });
-  it("should return an empty array", () => {
+  it("should return []", () => {
     expect(reduceDirections(["NORD", "SUD", "EST", "OUEST"])).to.deep.eq([]);
   });
   it("should return ['NORD','EST','SUD','OUEST']", () => {
