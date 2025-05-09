07cba4f85d78b866a32136a5e8a921068c338260
Mathieu <hei.tafita.2@gmail.com>
2025-05-09 15:45:36 +0300

    update with final solution

--- src/openingCeremony.js
+++ src/openingCeremony.js
@@ -1,14 +1,5 @@
 export const getLastDelegation = (delegations) => {
-  let delegationInfo = [];
-  for (const delegation of delegations) {
-    const [name, distanceToTarget, velocity] = delegation.split(" ");
-    delegationInfo.push({
-      name: name,
-      timeTakenToTarget: parseInt(distanceToTarget) / parseInt(velocity),
-    });
-  }
-  delegationInfo.sort(
-    (a, b) => a["timeTakenToTarget"] - b["timeTakenToTarget"]
-  );
-  return delegationInfo[delegationInfo.length - 1]["name"];
+  const duration = delegations.map((el, i) => Number(el.split(" ")[1]) / Number(el.split(" ")[2]));
+  return delegations.map((el, i) => el.split(" "))[duration.indexOf(Math.max(...duration))][0];
 };
+export default getLastDelegation;
